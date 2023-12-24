72382f6de3a552209ffdc69db3849554df1b9eeb
JTGlez <jorjeluigi1@comunidad.unam.mx>
2023-12-24 00:18:22 -0600

    Initial setup

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import * as React from "react";
+import ModeToggle from "@/components/ThemeToggle";
+
+export default function Home() {
+
+  return (
+    <>
+      <ModeToggle />
+    </>
+  )
+}
